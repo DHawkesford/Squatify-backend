adccdbe619dc685c5785cacac4dcc8bfebade8af
Philip Kaminski <philip.doge.kaminski@gmail.com>
2022-07-03 09:52:23 +0100

    create and populate scripts working

--- db/scripts/squatLog/populateTable.js
+++ db/scripts/squatLog/populateTable.js
@@ -1,12 +1,13 @@
 import db from "../../connection.js";
 
+//The populate script is for dev purposes only- production version will arive with an empthy table for the user to
+//populate themselves.
+let sqlString = `INSERT INTO squat_log (no_squats) VALUES (15), (18), (22) RETURNING *;`;
 async function populateTable() {
-  const response = await db.query(
-    `INSERT INTO squat_log  `
-  );
-  
+  const response = await db.query(sqlString);
+
   console.log(response);
-  
+
   db.end();
 }
 
